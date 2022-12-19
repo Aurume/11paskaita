339fa0eda28fb787c3da8474bfe11af0d711ad71
Aurelija Petrauskaite <aurelija35@gmail.com>
2022-12-19 12:22:44 +0200

    Kolkas tik sąrašo sukūrimas

---
+++ uzduotis2.py
@@ -0,0 +1,14 @@
+# Padaugintų visus sąrašo skaičius iš 10 ir atspausdintų
+# Atrinktų iš sąrašo skaičius, kurie dalinasi iš 7 ir atspausdintų
+# Pakeltų visus sąrašo skaičius kvadratu ir atspausdintų. Šį sąrašą (list masyvą) priskirti naujam kintamajam.
+# Su kvadratų sąrašu (nauju kintamuoju) atliktų šiuos veiksmus: atspausdintų sumą, mažiausią ir didžiausią skaičių, vidurkį, medianą
+# Surūšiuotų ir atspausdintų kvadratų sąrašą atbulai
+#
+# Naudoti map, filter arba comprehension, sum, min, max, mean, median, %
+from statistics import mean, median
+
+
+# Sukurtų sąrašą iš skaičių nuo 0 iki 50
+
+skaiciai = range(0,51)
+print(list(skaiciai))
